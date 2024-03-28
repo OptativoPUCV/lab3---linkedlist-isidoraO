2042035829b0d141913a226fa3f817074c2f5ceb
Your Name <you@example.com>
2024-03-28 22:20:56 +0000

    new try

--- list.c
+++ list.c
@@ -121,7 +121,7 @@ void * popCurrent(List * list)
     {
       list->head = list->current->next;
       if(list->head)
-        list->head->prev = NULL;  
+        printf("Hello");
     }
   return NULL;
 }
